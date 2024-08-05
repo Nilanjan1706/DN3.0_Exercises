7213ac08ac5f7ddcd825f3ede1a3eb077a4a1b1c
Nilanjan1706 <91783338+Nilanjan1706@users.noreply.github.com>
2024-08-05 21:18:39 +0530

    Delete Week_2_Exercises/Spring/librarymanagement directory

--- Week_2_Exercises/Spring/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void printRepository() {
-        System.out.println("BookRepository is called.");
-    }
-}
--- Week_2_Exercises/Spring/librarymanagement/src/main/java/com/library/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void printService() {
-        System.out.println("BookService is called.");
-    }
-}
